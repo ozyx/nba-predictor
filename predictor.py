9c74c3502747dae798f6a412198712694770fe00
Jarrett <jarrettchien@hotmail.com>
2020-03-07 18:43:18 -0800

    update predictor

--- predictor.py
+++ predictor.py
@@ -1,6 +1,20 @@
 import pandas as pd
-import xgboost as xgb
+from decimal import Decimal
 
 data = pd.read_csv("nba.games.stats.csv")
 print(data.head())
 
+matches = data.shape[0]
+team = "LAC"
+wins = len(data.query(f'Team=="{team}" & Home=="Home" & WINorLOSS =="W"'))
+losses = len(data.query(f'Team=="{team}" & Home=="Home" & WINorLOSS =="L"'))
+homegames = len(data.query(f'Team=="{team}" & Home=="Home"'))
+print(homegames)
+
+homewinrate = (wins / homegames) * 100
+homewinrate = round(homewinrate,2)
+#(float(wins) / (homegames)) * 100
+
+#print("Total number of matches: " + str(matches))
+print(f"Number of matches won by {team} when at Home: {wins}")
+print(f"Win rate of {team} when at Home: {homewinrate}%")
